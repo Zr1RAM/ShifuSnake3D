0eee28416a5258dfb98b891afa313bcf5e9dccc8
Zr1RAM <sriram.varadarajulu@gmail.com>
2021-02-02 11:23:53 +0530

    snake head controller and snake head prefab

---
+++ Assets/Scripts/Snake/SnakeHeadController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeHeadController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Wall")
+        {
+            SnakeRecoilMovement();
+        }
+    }
+    void SnakeRecoilMovement()
+    {
+
+    }
+}
